d87360a1f820d44fa33db863389b8846c87b38f6
SimplyLizzy <elingu94@gmail.com>
2022-02-13 13:05:19 -0600

    heat map updates

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+# https://home.openweathermap.org/api_keys
+weather_api_key = "86cb9788ec874ce3961bfd73a5fc6302"
+
+# Google API Key
+g_key = "AIzaSyBxg1M16K9FYZkpouvRc4xE6WwwJe1nKG8"
